3b4ae07c68a603addf4db5ba16171fda9e2ce469
1115326582 <1115326582@qq.com>
2019-12-05 12:19:35 +0800

    vue-music

--- src/api/rank.js
+++ src/api/rank.js
@@ -1,33 +1,34 @@
-import jsonp from 'common/js/jsonp'
-import {
-  commonParams,
-  options
-} from './config'
-import axios from 'axios'
-const debug = process.env.NODE_ENV !== 'production'
+import jsonp from 'common/js/jsonp';
+import { commonParams, options } from './config';
+import axios from 'axios';
+const debug = process.env.NODE_ENV !== 'production';
 
 export function getTopList () {
-  const url = debug ? '/api/getTopList' : 'http://www.yeelei.top/music/api/getTopList'
-  return axios.post(url, {
-    'req_0': {
-      'module': 'musicToplist.ToplistInfoServer',
-      'method': 'GetAll',
-      'param': {}
-    },
-    'comm': {
-      'g_tk': 5381,
-      'uin': 0,
-      'format': 'json',
-      'ct': 23,
-      'cv': 0
-    }
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  })
+  const url = debug
+    ? '/api/getTopList'
+    : 'http://www.yeelei.top/music/api/getTopList';
+  return axios
+    .post(url, {
+      req_0: {
+        module: 'musicToplist.ToplistInfoServer',
+        method: 'GetAll',
+        param: {}
+      },
+      comm: {
+        g_tk: 5381,
+        uin: 0,
+        format: 'json',
+        ct: 23,
+        cv: 0
+      }
+    })
+    .then(res => {
+      return Promise.resolve(res.data)
+    })
 }
 
 export function getMusicList (topid) {
-  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg';
 
   const data = Object.assign({}, commonParams, {
     topid,
